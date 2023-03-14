9f1fd55a46127313b367d293e2a37a46ca12617d
Kalest3 <gatchmew@gmail.com>
2023-03-14 17:56:49 -0300

    update PAM, DS

---
+++ 2etimds2022/Desenvolvimento de Sistemas/Scripts/QuatroOperacoes/QuatroOperacoes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuatroOperacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, soma, sub, mult, div;
+
+            Console.WriteLine("Digite um número: ");
+            n1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite outro: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            soma = n1 + n2;
+            sub = n1 - n2;
+            mult = n1 * n2;
+
+            Console.WriteLine("A soma entre os dois números é: " + soma);
+            Console.WriteLine("A subtração entre os dois números é: " + sub);
+            Console.WriteLine("A multiplicação entre os dois números é: " + mult);
+
+            if (n2 == 0)
+            {
+                Console.WriteLine("Divisão por 0 é impossível.");
+            }
+            else
+            {
+                div = n1 / n2;
+                Console.WriteLine("A divisão entre os dois números é: " + div);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 2etimds2022/Desenvolvimento de Sistemas/Scripts/SomaDoisNumeros/SomaDoisNumeros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaDoisNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, soma;
+
+            Console.WriteLine("digite o primeiro valor: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo valor:");
+            n2 = int.Parse(Console.ReadLine());
+            soma = n1 + n2;
+            Console.WriteLine("o resultado sera:" + soma);
+            Console.ReadLine();
+
+        }
+    }
+}
